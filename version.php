c23e80b5a5e860cd5a01979d2752b480e759b47c
Eduardo Exequiel Sepulveda Herrera <e.esepulvedah.github@gmail.com>
2026-04-23 00:38:36 -0400

    Add files via upload

---
+++ version.php
@@ -0,0 +1,8 @@
+<?php
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->component = 'local_loginsync';
+$plugin->version   = 2026042201;
+$plugin->requires  = 2022041900; // Moodle 4.0+
+$plugin->maturity  = MATURITY_STABLE;
+$plugin->release   = '1.0';
